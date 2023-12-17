e89ccba3253c25357079e449af6e5e90f0e908d2
Oleg Kireev <kuru@mail.ru>
2023-12-17 22:04:12 +0300

    Task-8 add start button

--- src/script.js
+++ src/script.js
@@ -1,6 +1,10 @@
 const dino = document.getElementById("dino");
 const cactus = document.getElementById("cactus");
 const score = document.getElementById("score");
+const start = document.getElementById("start");
+const btnStart = document.getElementById("btnStart");
+const divJump = document.getElementById("divJump");
+const btnJump = document.getElementById("btnJump");
 const audioJump = new Audio();
 audioJump.src = 'mp3/jump.mp3';
 const audioRig1 = new Audio();
@@ -10,9 +14,23 @@ audioKick.src = 'mp3/kick.mp3';
 let count = 0;
 let jumpCount = 0;
 
+window.addEventListener("load", function (event) {
+    divJump.style.display = 'none';
+});
+
 document.addEventListener("keydown", function (event) {
     jump();
-})
+});
+
+btnStart.addEventListener("click", function (event) {
+    divJump.style.display = 'block';
+    start.style.display = 'none';
+    cactus.className = 'cactus';
+});
+
+btnJump.addEventListener("click", function (event) {
+    jump();
+});
 
 function jump() {
     if (dino.classList != "jump") {
@@ -27,14 +45,20 @@ function jump() {
 }
 
 let isAlive = setInterval(function () {
-    count++;
+
+    if (cactus.className == 'cactus') {
+        count++;
+    }
+
     let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
     let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
     if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140) {
         count = 0;
         jumpCount = 0;
         audioKick.play();
-        alert("Убился об кактус");
+        cactus.className = 'cactusNone';
+        divJump.style.display = 'none';
+        start.style.display = 'block';
     }
 
     score.innerHTML = "Очки: " + count + " Прыжки: " + jumpCount;
